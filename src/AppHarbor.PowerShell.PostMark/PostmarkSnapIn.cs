820f9759bf12251353736e5c037b8de16d0e49bc
Michael Friis <friism@gmail.com>
2011-01-10 17:55:30 -0800

    implement PostmarkSnapIn

--- src/AppHarbor.PowerShell.PostMark/PostmarkSnapIn.cs
+++ src/AppHarbor.PowerShell.PostMark/PostmarkSnapIn.cs
@@ -1,7 +1,44 @@
-﻿
+﻿using System.Management.Automation;
+
 namespace AppHarbor.PowerShell.Postmark.Commands
 {
-	class PostmarkSnapIn
+	class PostmarkSnapIn : PSSnapIn
 	{
+		public PostmarkSnapIn()
+			: base()
+		{
+		}	
+
+		public override string Name
+		{
+			get
+			{
+				return "AppHarbor.PowerShell.Postmark";
+			}
+		}
+
+		public override string Vendor
+		{
+			get
+			{
+				return "AppHarbor";
+			}
+		}
+
+		public override string VendorResource
+		{
+			get
+			{
+				return "PostmarkSnapIn,AppHarbor";
+			}
+		}
+
+		public override string Description
+		{
+			get
+			{
+				return "This is a snapin that sends email using the Postmark API";
+			}
+		}
 	}
 }
