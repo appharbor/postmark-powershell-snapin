2b5afb5a0561bc8d2e1e69510a9427a828f393b8
Michael Friis <friism@gmail.com>
2011-01-10 18:03:33 -0800

    Implement PostarkClient

--- src/AppHarbor.PowerShell.PostMark/PostmarkClient.cs
+++ src/AppHarbor.PowerShell.PostMark/PostmarkClient.cs
@@ -1,7 +1,30 @@
-﻿
+﻿using System.Management.Automation;
+using PM = PostmarkDotNet;
+
 namespace AppHarbor.PowerShell.Postmark.Commands
 {
-	public class PostmarkClient
+	[Cmdlet(VerbsCommon.Get, "PostmarkClient")]
+	public class PostmarkClient : Cmdlet
 	{
+		private PM.PostmarkClient postmarkClient;
+
+		[Parameter(Position = 0)]
+		[ValidateNotNullOrEmpty]
+		public string ServerToken
+		{
+			get;
+			set;
+		}
+
+		protected override void ProcessRecord()
+		{
+			postmarkClient = new PM.PostmarkClient(ServerToken);
+			WriteObject(postmarkClient);
+		}
+
+		public PM.PostmarkResponse SendEmail(PM.PostmarkMessage message)
+		{
+			return postmarkClient.SendMessage(message);
+		}
 	}
 }
