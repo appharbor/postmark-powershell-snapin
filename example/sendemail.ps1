7a953be6fbc9d002ba872c26699f43172263e50e
Michael Friis <friism@gmail.com>
2011-01-12 21:23:53 -0800

    remove unused function

--- example/sendemail.ps1
+++ example/sendemail.ps1
@@ -13,16 +13,6 @@ function Load-Module([string] $assembly_lib_path)
     Import-Module $full_lib_path
 }
 
-function Non-Blocking-Assembly-Load([string] $full_lib_path)
-{
-    $fileStream = ([System.IO.FileInfo] (Get-Item $full_lib_path)).OpenRead();
-    $assemblyBytes = new-object byte[] $fileStream.Length
-    $fileStream.Read($assemblyBytes, 0, $fileStream.Length);
-    $fileStream.Close();
-
-    [System.Reflection.Assembly]::Load($assemblyBytes);
-}
-
 function Load-Postmark-Lib()
 {
     Load-Module "Hammock.dll"
